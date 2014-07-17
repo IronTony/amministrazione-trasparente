5bd9e1219d1cb362c933267c39edf521c238f96b
IronTony <zimzum4@gmail.com>
2014-07-17 15:00:06 +0200

    Update shortcodes-php-archive.php
    Renamed .show and .hide with .showArchive and .hideArchive to prevent conflict with bootstrap.css

--- shortcodes/shortcodes-php-archive.php
+++ shortcodes/shortcodes-php-archive.php
@@ -5,18 +5,18 @@ $get_at_option_showarchivedesc = get_option('at_option_showarchivedesc'); ?>
 <style type="text/css">
 .row {vertical-align: top; height:auto !important;}
 .list {display:none;}
-.show {display:none;}
-.hide:target + .show {display:inline;}
-.hide:target {display:none;}
-.hide:target ~ .list {display:inline;}
-@media print {.hide, .show {display:none;}}
+.showArchive {display:none;} /* Fixed Bootstrap conflict CSS */
+.hideArchive:target + .showArchive {display:inline;} /* Fixed Bootstrap conflict CSS */
+.hideArchive:target {display:none;} /* Fixed Bootstrap conflict CSS */
+.hideArchive:target ~ .list {display:inline;} /* Fixed Bootstrap conflict CSS */
+@media print {.hideArchive, .showArchive {display:none;}} /* Fixed Bootstrap conflict CSS */
 </style>
 <?php } ?>
 
 <div class="row" style="font-size:0.8em;text-align:center;">
 
 <?php if ($get_at_option_showarchivedesc == '0') {?>
-<a href="#hideARCHIVE" class="hide" id="hideARCHIVE">[+] Info Normativa</a><a href="#showARCHIVE" class="show" id="showARCHIVE">[-] Info Normativa</a> &bull; <?php } ?>
+<a href="#hideARCHIVE" class="hideArchive" id="hideARCHIVE">[+] Info Normativa</a><a href="#showARCHIVE" class="showArchive" id="showARCHIVE">[-] Info Normativa</a> &bull; <?php } ?>
 
 <a href="<?php echo get_permalink(get_option('at_option_id')); ?>" title="Torna al sommario">Torna al sommario</a>
 
